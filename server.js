a4abfea1d81e9781e5be2dc5fc45b2cc493c3cae
sebastianbirk <sebastian.birk@outlook.com>
2020-07-05 12:10:33 +0200

    Add heroku ssl force

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const favicon = require('express-favicon');
+const forceSsl = require('force-ssl-heroku');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
@@ -9,6 +10,7 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(forceSsl);
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
